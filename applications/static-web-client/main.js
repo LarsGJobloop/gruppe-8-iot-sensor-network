4fa6b7576d84fca9a2487ec200dc2584e4838484
larsgjobloop <lars.gunnar@jobloop.no>
2024-06-21 10:17:16 +0000

    fix client code

--- applications/static-web-client/main.js
+++ applications/static-web-client/main.js
@@ -1,12 +1,34 @@
-// Hent data
-const response = await fetch("http://localhost:3000/reports")
-const data = await response.json()
+// JSDoc can be used to document the various types
+// to help you when developing
+// https://jsdoc.app/tags-typedef
 
-// Log data til konsollen
-console.log(response)
-console.log(data)
+/**
+ * The shape of a weather report from the Web API
+ * 
+ * @typedef {{
+ * sensorId: number,
+ * measurementDate: string,
+ * temperature: number
+ * }} WeatherReport
+ */
 
-// Bruk dataen til å oppdatere html (Dokumentet DOMet)
-const element = document.getElementById("tempurature")
-console.dir(element)
-element.textContent = data.temperature
+async function getLatestReport() {
+  // Hent data
+  const response = await fetch("http://localhost:3000/reports")
+
+  /**
+   * @type {Array<WeatherReport>}
+   */
+  const weatherReports = await response.json()
+  const lastWeatherReport = weatherReports[weatherReports.length - 1]
+  
+  // Bruk dataen til å oppdatere html (Dokumentet DOMet)
+  const temperatureElement = document.getElementById("tempurature")
+  temperatureElement.textContent = lastWeatherReport.temperature
+  
+  const timestampElement = document.getElementById("time")
+  timestampElement.textContent = lastWeatherReport.measurementDate
+}
+
+getLatestReport()
+setInterval(getLatestReport, 5 * 1000)
