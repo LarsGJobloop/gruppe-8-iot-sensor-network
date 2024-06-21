49d5e909ca07cf61c551ce613ef08c8444cf2d91
larsgjobloop <lars.gunnar@jobloop.no>
2024-06-21 08:56:03 +0000

    fix getAllReports endpoint

--- applications/server/main.js
+++ applications/server/main.js
@@ -11,7 +11,7 @@ let reportId = 0
  */
 function getAllReports(request, response) {
   // Convert to network format
-  const data = JSON.stringify(report)
+  const data = JSON.stringify(reports)
   
   // Send the resulting package
   response.end(data)
